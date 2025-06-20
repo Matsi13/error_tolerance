3b58d123496e4b43ec1945462d6ddad7b49d59fd
李金熹 <ljx23@mails.tsinghua.edu.cn>
2025-06-20 19:10:32 +0800

    feat: modified error_fit function in matlab, normalize the number of possible optimals to 1 when the error is 0

--- matlab/error_fit.m
+++ matlab/error_fit.m
@@ -1,7 +1,9 @@
 close all; clear; clc
-load ../out/output/0044_error_curve.txt
-TFLOPs_error = X0044_error_curve(:, 1);
-num = X0044_error_curve(:, 2);
+load ../out/output/0001_error_curve.txt
+TFLOPs_error = X0001_error_curve(:, 1);
+num = X0001_error_curve(:, 2);
+
+num = num./num(1);
 
 plot(TFLOPs_error, num);
 % plot3(TFLOPs_error, paramsize_error, num);
--- matlab/visualize.m
+++ matlab/visualize.m
@@ -1,6 +1,6 @@
 close all; clear; clc
-load ../out/output/0045_wafer.txt
-Generate_output = X0045_wafer;
+load ../out/output/0001_wafer.txt
+Generate_output = X0001_wafer;
 TFLOPS = Generate_output(:, 1);
 SRAM_capacity = Generate_output(:, 2);
 DRAM_capacity = Generate_output(:, 3);
