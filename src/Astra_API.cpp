fd405346d62be30843a380f2a5faba024db9fd0a
李金熹 <ljx23@mails.tsinghua.edu.cn>
2025-06-25 22:03:54 +0800

    debug: modified astra_API.cpp

--- src/Astra_API.cpp
+++ src/Astra_API.cpp
@@ -57,11 +57,17 @@ void astra_API(float freq, float off_chip_bandwidth, float TFLOPs, float model_s
         float forward_off_chip_access_time = forward_off_chip_access_size / off_chip_bandwidth;
         float forward_access_time = max(forward_DRAM_access_time, forward_off_chip_access_time);
 
-        float forward_time = max(forward_compute_time, forward_access_time);
-        float forward_communication_size = traffic;
-        float input_gradient_time = forward_compute_time; // no kv_cache in backward process. backward process is like prefill
-        float weight_gradient_time = forward_compute_time;
-        float weight_communication_size = model_size_per_die * (rows - 1);
+
+        int forward_cycle = time2cycle(max(forward_compute_time, forward_access_time));
+        int forward_communication_size = int(traffic);
+        int input_gradient_cycle = forward_compute_cycle; // no kv_cache in backward process. backward process is like prefill
+        int weight_gradient_cycle = forward_compute_cycle;
+        int weight_communication_size = ceil(model_size_per_die * (rows - 1));
+
+
+        
+        
+        
     }
         
 
